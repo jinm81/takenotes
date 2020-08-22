cece861a024c88d1ab30e5ba482c31b92f709ee5
hojin Mun <jin1281@gmail.com>
2020-08-21 21:32:33 -0400

    updated

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fs = require("fs");
+
+
+var app = express();
+var PORT = process.env.PORT || 5000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+
+
+require("./routing/htmlroutes")(app);
+require("./routing/apiroutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
