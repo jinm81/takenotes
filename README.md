afa513f7d26c98c7b5666b24932c667826ed6122
hojin Mun <jin1281@gmail.com>
2020-08-22 21:31:09 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# takenotes
+Note Taker
+An application that can be used to write, save, and delete notes. This application uses an express backend and can save and retrieve note data from a JSON file.
+
+For users that need a little help organizing thoughts, keeping track of tasks you need to complete or just to keep track of a lot of information. Life moves fast and sometimes it's easy for us to forget or simply be unable to recall some of those important "something's". Being able to take persistent notes allows users to have those written "something's" available when and where needed.
+
+video of NoteTaker 
+https://drive.google.com/file/d/1NBDHLnK3iyuXqDXwiBUk8xJk4cBbQIKz/view
--- server.js
+++ server.js
@@ -1,19 +1,62 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
+const app = express();
+var PORT = process.env.PORT || 8000;
+const mainDir = path.join(__dirname, "/public");
 
-var app = express();
-var PORT = process.env.PORT || 5000
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/assets", express.static("./assets"));
 
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
 
-require("./route/htmlroute")(app);
-require("./route/apiroute")(app);
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
+app.get("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.post("/api/notes", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let uniqueID = savedNotes.length.toString();
+  newNote.id = uniqueID;
+  savedNotes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  console.log("saved note db.json. Content: ", newNote);
+  res.json(savedNotes);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+
+  savedNotes = savedNotes.filter((currNote) => {
+    return currNote.id != noteID;
+  });
+
+  for (currNote of savedNotes) {
+    currNote.id = newID.toString();
+    newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
 });
